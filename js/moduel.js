eba26c32eec6f97ab02cbbf0edd074ce1a796cd0
Clark Ozanne <clarkozanne@gmail.com>
2016-02-15 10:51:15 -0800

    routes

--- js/moduel.js
+++ js/moduel.js
@@ -3,8 +3,18 @@
 var app = angular.module("theApp", ['ui-router']);
 
 app.controller('mainControl', ['$scope','$https' function($scope, $https){
+  $scope.listItems;
+  $scope.checkItOut = function(){
+    $https.get()
+    .then(function(listItems){
+      
+    })
+  }
+
   $scope.newListItem = function(){
     var newListItem = $scope.listItem
     $https.post('/users/new', {listItem: newListItem})
+    .then(checkItOut())
   }
+
 }])
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+// var List.mongoose.model
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/new', function(req, res, next) {
+  listItem.create(req.body, function(err, newListItem))
+  res.send('worked');
 });
 
 module.exports = router;
