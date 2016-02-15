bde7e6ce12ad9fe4427ccdd90f12aee582bc96a4
Clark Ozanne <clarkozanne@gmail.com>
2016-02-15 10:39:09 -0800

    adding routes

--- js/moduel.js
+++ js/moduel.js
@@ -2,8 +2,9 @@
 
 var app = angular.module("theApp", ['ui-router']);
 
-app.config(function($stateProvider, $urlRouteProvider){
-  $stateProvider
-  .state()
-  $urlRouteProvider.otherwise("/")
-})
+app.controller('mainControl', ['$scope','$https' function($scope, $https){
+  $scope.newListItem = function(){
+    var newListItem = $scope.listItem
+    $https.post('/users/new', {listItem: newListItem})
+  }
+}])
